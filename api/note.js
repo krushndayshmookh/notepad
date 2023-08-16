96ba18b8aefd921585dbfc6c3d8001d144da9a31
Krushn Dayshmookh <krushn.dayshmookh.ext@siemens.com>
2023-08-17 03:34:13 +0530

    fix request and response

--- api/note.js
+++ api/note.js
@@ -2,15 +2,14 @@ import { sql } from '@vercel/postgres'
 
 export default async function handler(request, response) {
   if (request.method === 'POST') {
-    const noteBody = request.body
-    const { rows } =
-      await sql`UPDATE notes SET body = ${noteBody} WHERE id = 1 RETURNING *`
-    return response.status(200).send('OK')
+    const { body } = request.body
+    await sql`UPDATE notes SET body = ${body} WHERE id = 1 RETURNING *`
+    return response.status(200).send({ success: true })
   }
 
   if (request.method === 'GET') {
     const { rows } = await sql`SELECT * FROM notes WHERE id = 1`
-    return response.status(200).json(rows[0].body)
+    return response.status(200).json({ body: rows[0].body })
   }
 
   return response.status(405).end()
